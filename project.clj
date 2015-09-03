847ecc4ca1a2f211ae52ff3a0b31d0595fbeb4fc
Toby Smyth <tobs169@gmail.com>
2015-09-03 07:16:46 +0100

    lein-ring server

--- project.clj
+++ project.clj
@@ -3,4 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]])
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :plugins [[lein-ring "0.9.6"]]
+  :ring {:handler toby-site-clj.core/handler})
--- src/toby_site_clj/core.clj
+++ src/toby_site_clj/core.clj
@@ -1,6 +1,7 @@
 (ns toby-site-clj.core)
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn handler
+  [request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello world"})

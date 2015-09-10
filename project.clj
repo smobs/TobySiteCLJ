bc8de7194827ae9227c4dc2b03ac36f4d66b9762
Toby Smyth <tobs169@gmail.com>
2015-09-10 15:44:02 +0100

    Manually spin up server

--- project.clj
+++ project.clj
@@ -4,7 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.7.0"]
+                 [ring/ring-jetty-adapter "1.1.0"]
                  [org.clojure/data.json "0.2.6"]]
+  :main ^:skip-aot toby-site-clj.core
   :plugins [[lein-ring "0.9.6"]]
   :profiles {:uberjar {:aot :all}}
   :uberjar-name "toby-site-clj-standalone.jar"
--- src/toby_site_clj/core.clj
+++ src/toby_site_clj/core.clj
@@ -1,10 +1,17 @@
 (ns toby-site-clj.core
   [:require 
    [cluedo.core :as cluedo]
-   [clojure.data.json :as json]])
+   [clojure.data.json :as json]
+   [ring.adapter.jetty :as jetty]]
+  (:gen-class))
 
 (defn handler
   [request]
   {:status 200
    :headers {"Content-Type" "text/html"}
    :body (json/write-str cluedo/new-cluedo)})
+
+(defn -main 
+  [& port]
+  (let [port (Integer. (or port (System/getenv "PORT")))]
+    (jetty/run-jetty #'handler {:port port })))
